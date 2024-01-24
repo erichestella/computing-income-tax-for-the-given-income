be444f45d473b690c46255a1d59344480a3eb9a1
erichestella <erichestella05@gmail.com>
2024-01-24 22:18:19 +0800

    income is less than or equal to 20000

--- income tax.py
+++ income tax.py
@@ -10,3 +10,8 @@ tax_numerical = 0
 #the income if it is less than 10000
 if income <= 10_000:
    tax_digit = 0
+
+#the income if its less than equal to 20000
+elif 10_000 < income <= 20_000:
+    first_income= income - 10_000
+    tax_digit = first_income * 0.1
