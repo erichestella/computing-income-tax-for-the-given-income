61e3e307285d6bde4ef8dbcf879fefb67d44d89d
erichestella <erichestella05@gmail.com>
2024-01-24 21:23:07 +0800

    input ypur chosen income

--- calcuting income tax.py
+++ calcuting income tax.py
@@ -1,5 +1,8 @@
 #title of the code
 print('\nCALCULATE THE INCOME TAX FOR THE GIVEN INCOME.\n')
 
+#it inputs your designated income
+income_numerical = int(input("YOUR INCOME : "))
+
 #variable for the tax
 tax_digits = 0
