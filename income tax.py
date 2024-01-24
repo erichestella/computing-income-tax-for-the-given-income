1ba82a159948bdee6e903aa0ce107eb7d1b042a1
erichestella <erichestella05@gmail.com>
2024-01-24 22:36:37 +0800

    displaying the total tax

--- income tax.py
+++ income tax.py
@@ -1,6 +1,7 @@
 #title of the code
 print('\nCOMPUTING THE INCOME TAX FOR THE GIVEN INCOME.\n')
 
+#given income tax and rate
 print('\nTAXABLE INCOME:\nFirst $10,000\nNext $10,000\nThe remaining')
 print('\nRate (in %):\n    0\n    10\n    20\n')
 
@@ -29,4 +30,4 @@ else:
      tax_digit+= (income- 20_000)* 0.2
 
 #it displays the total income
-print('\nTOTAL TAX THAT IS NEED TO PAY IS','$',int(tax_digit), '\n')
+print('\nTHE TOTAL TAX THAT YOU NEED TO PAY IS','$',int(tax_digit), '\n')
