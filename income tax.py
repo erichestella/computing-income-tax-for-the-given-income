4f0e9ff6b3b0e3fd3d4fc39c7680da0ad1d31ab5
erichestella <erichestella05@gmail.com>
2024-01-24 22:13:30 +0800

    the income if its less than and a variable for tax

--- income tax.py
+++ income tax.py
@@ -3,3 +3,10 @@ print('\nCOMPUTING THE INCOME TAX FOR THE GIVEN INCOME.\n')
 
 #your designated income
 income = int(input('YOUR INCOME : '))
+
+#variable to illustrate the tax
+tax_numerical = 0
+
+#the income if it is less than 10000
+if income <= 10_000:
+   tax_digit = 0
